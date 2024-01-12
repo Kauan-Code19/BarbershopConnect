299bcb8965a9fae8244c701c9495b3a7d5aff894
Kauan-Code19 <pkauprofissional@gmail.com>
2024-01-12 15:25:19 -0300

    Projeto Criado

---
+++ src/main/java/com/BarbershopConnect/BarbershopConnect/BarbershopConnectApplication.java
@@ -0,0 +1,13 @@
+package com.BarbershopConnect.BarbershopConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbershopConnectApplication.class, args);
+	}
+
+}
