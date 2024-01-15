2c4e72a99571ddee41023264cea1afce9b683edf
Kauan-Code19 <pkauprofissional@gmail.com>
2024-01-15 19:32:14 -0300

    Resposta do Controller customizada.

--- src/main/java/com/BarbershopConnect/BarbershopConnect/controllers/ClienteController.java
+++ src/main/java/com/BarbershopConnect/BarbershopConnect/controllers/ClienteController.java
@@ -3,10 +3,14 @@ package com.BarbershopConnect.BarbershopConnect.controllers;
 import com.BarbershopConnect.BarbershopConnect.dto.ClienteDTO;
 import com.BarbershopConnect.BarbershopConnect.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/cliente")
@@ -16,7 +20,12 @@ public class ClienteController {
     private ClienteService clienteService;
 
     @PostMapping
-    public ClienteDTO cadastro(@RequestBody ClienteDTO clienteDTO) {
-        return clienteService.cadastrar(clienteDTO);
+    public ResponseEntity<ClienteDTO> cadastro(@RequestBody ClienteDTO clienteDTO) {
+        clienteDTO = clienteService.cadastrar(clienteDTO);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(clienteDTO.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(clienteDTO);
     }
 }
