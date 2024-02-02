d6c13d589de7c4e1feff2c7137294bd1ab0fe3d0
Kauan-Code19 <pkauprofissional@gmail.com>
2024-02-02 17:17:33 -0300

    Resolvendo conflito de merge.

--- src/main/java/com/BarbershopConnect/BarbershopConnect/dto/ClienteDTO.java
+++ src/main/java/com/BarbershopConnect/BarbershopConnect/dto/ClienteDTO.java
@@ -1,6 +1,9 @@
 package com.BarbershopConnect.BarbershopConnect.dto;
 
-import com.BarbershopConnect.BarbershopConnect.entities.Barbeiro;
+import com.BarbershopConnect.BarbershopConnect.entities.Endereco;
+import jakarta.persistence.Embedded;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -9,26 +12,25 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
-public class BarbeiroDTO {
+public class ClienteDTO {
     private Long id;
 
     @Size(min = 3, max = 80, message = "O Campo precisa ter de 3 a 80 caracteres")
     @NotBlank(message = "Campo Requerido")
     private String nome;
 
-    @Size(min = 10, max = 200, message = "A descrição deve ter entre 10 e 200 caracteres.")
-    private String descricao;
+    @Email
+    private String email;
+
+    @Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).*$",
+            message = "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula e um número.")
+    private String senha;
+
+    @Valid
+    @Embedded
+    private Endereco endereco;
 
     @Pattern(regexp = "^\\(?(\\d{2})\\)?[-.\\s]?\\d{4,5}[-.\\s]?\\d{4}$", message = "Formato de número de telefone inválido.")
     private String contato;
-
-    private BarbeariaResponseDTO barbearia;
-
-    public BarbeiroDTO(Barbeiro entity) {
-        id = entity.getId();
-        nome = entity.getNome();
-        descricao = entity.getDescricao();
-        contato = entity.getContato();
-        barbearia = new BarbeariaResponseDTO(entity.getBarbearia());
-    }
 }
